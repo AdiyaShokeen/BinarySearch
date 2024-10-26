466c1acae17a07878969328d86dd60fce008c890
Aditya Shokeen <82958883+AdiyaShokeen@users.noreply.github.com>
2024-10-26 09:59:38 +0000

    Recursive

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+
+"""
+BinarySearch Normal
+
 def BinarySearch(numList,val):
     start = 0
     end = len(numList) - 1
@@ -13,6 +17,24 @@ def BinarySearch(numList,val):
             end = mid - 1
 
     return -1
+"""
 
+# By recursion
+def BinarySearch(numList,val,left,right):
+    if right < left:
+        return -1
+        
+    mid = (left + right) // 2
+    mid_val = numList[mid]
+    if val == mid_val:
+        return mid
+    else:
+        if val < mid_val:
+            right = mid - 1
+        else:
+            left = mid + 1
+
+    return BinarySearch(numList,val,left,right)
+        
 num_list = [4,9,11,17,21,25,29,32,38]
-print(BinarySearch(num_list,29))
+print(BinarySearch(num_list,29,0,len(num_list)-1))
